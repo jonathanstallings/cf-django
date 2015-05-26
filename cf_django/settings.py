03c562592241314b2e1fe1f7e871fd71c013c51e
Jonathan Stallings <jonathanstallings@outlook.com>
2015-05-26 06:05:05 -0700

    Fix error in settings.py

--- cf_django/settings.py
+++ cf_django/settings.py
@@ -100,7 +100,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
 # Added the following for Heroku configuration:
 
