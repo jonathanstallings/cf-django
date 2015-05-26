eccfb4415dcc565539d88d8e9b8242055b4295da
Jonathan Stallings <jonathanstallings@outlook.com>
2015-05-26 03:00:18 -0700

    Improve layout

--- users/urls.py
+++ users/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.IndexView, name='index'),
-    url(r'^/add/$', views.AddView, name='add'),
-    url(r'^(?P<user_id>\d+)$', views.DetailView, name='detail'),
+    url(r'^$', views.index, name='index'),
+    url(r'^/add/$', views.add, name='add'),
+    url(r'^(?P<user_id>\d+)$', views.detail, name='detail'),
     url(r'^(?P<user_id>\d+)/edit/$', views.edit, name='edit'),
     url(r'^(?P<user_id>\d+)/delete/$', views.delete, name='delete'),
     url(r'^/about/$', views.about, name='about'),
--- users/views.py
+++ users/views.py
@@ -1,22 +1,21 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
-from django.views import generic
 
 from .models import User
 
 
-def IndexView(request):
+def index(request):
     context = {'user_list': User.objects.order_by('first_name')}
     return render(request, 'users/index.html', context)
 
 
-def DetailView(request, user_id=None):
+def detail(request, user_id=None):
     context = {'user': get_object_or_404(User, pk=user_id)}
     return render(request, 'users/detail.html', context)
 
 
-def AddView(request):
+def add(request):
     user = {
         'id': 0,
         'first_name': "",
